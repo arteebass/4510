56e46965a307f0771605481dbbfbf93dac17996c
Hunter Garrett <garretth@seattleu.edu>
2018-04-26 17:06:37 -0700

    Update proxy.h

--- proxy.h
+++ proxy.h
@@ -7,6 +7,7 @@
 using namespace std;
 #ifndef PROXY_H_
 #define PROXY_H_
+
 #include <algorithm>
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,36 +28,32 @@ using namespace std;
 #include <semaphore.h>
 #include <arpa/inet.h>
 #include <map>
+
 #define CRLF "\r\n"
 #define MSG_BUF_SIZE	1000000
 #define MAX_THREADS		30
 #define SEM_PERMISSIONS	0644
 
-// Multithreading
 int server_s;
+const char* errorMessage = "HTTP/1.0 500 'Internal Server Error'\r\n\r\n";
+static bool done = 0;
 std::queue<int>* socketQ;
 pthread_t* pool;
 pthread_mutex_t* count_mutex;
 sem_t* job_queue_count;
 char SEM_NAME[] = "sem";
 
-static bool done = 0;
-
-//Error Handling
 void error(const char *err) {
 	perror(err);
 	exit(EXIT_FAILURE);
 }
 
-//Error Msg
-const char* errorMessage = "HTTP/1.0 500 'Internal Server Error'\r\n\r\n";
-
-
-//Function definitions
 void receiveMsg(int s, int size, char *ptr, bool headerOnly);
 void sendMsg(int s, int size, char *ptr);
 void closeSocket(int sock, char* &Buffer);
 void* parse(void* threads);
 void initSynchronization();
 void initThreadPool();
+void setupSigHandlers();
+void termination_handler(int signum);
 #endif
